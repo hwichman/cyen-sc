d6e777bf1b3312084a46df8eab5c5d1470c49a01
hwichman <haleyrainn@gmail.com>
2020-02-13 18:35:08 -0600

    Working BT w/ temp

--- SWANS/SWANS/SWANS.ino
+++ SWANS/SWANS/SWANS.ino
@@ -1,15 +1,26 @@
-#include <TemperatureSensor.h>
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
 char junk;
 String inputString="";
+OneWire oneWire(3);
+DallasTemperature sensors(&oneWire);
+
+DeviceAddress thermometer = { 0x28, 0x5A, 0x22, 0x5E, 0x1A, 0x19, 0x01, 0x69 };
 void setup()                    // run once, when the sketch starts
 {
  Serial.begin(9600);            // set the baud rate to 9600, same should be of your Serial Monitor
  pinMode(13, OUTPUT);
  Serial.println("you are connected");
+ sensors.begin();
+ sensors.setResolution(thermometer, 10);
 }
 void loop()
 {
+  sensors.requestTemperatures();
+  Serial.println("temperature: ");
+  printTemperature(thermometer);
+  //Serial.println(String(TempC));
   if(Serial.available()){
    
   while(Serial.available())
@@ -20,8 +31,19 @@ void loop()
     Serial.println(inputString);
     while (Serial.available() > 0)  
     { junk = Serial.read() ; }      // clear the serial buffer
-    float TempC = TemperatureSensor.getTemperature();
-    Serial.println(String(TempC));
     inputString = "";
+  }
+}
+
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+ if (tempC == -127.00) {
+    Serial.print("Error getting temperature");
+  } else {
+    Serial.print("C: ");
+    Serial.print(tempC);
+    Serial.print(" F: ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
   }
 }
